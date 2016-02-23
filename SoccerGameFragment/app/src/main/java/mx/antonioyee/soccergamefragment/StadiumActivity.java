205eee46390af8586fa23438916f4e5ca854748a
ANTONIO YEE <yee.antonio@gmail.com>
2016-02-23 11:53:11 -0600

    marcadores

--- SoccerGameFragment/app/src/main/java/mx/antonioyee/soccergamefragment/StadiumActivity.java
+++ SoccerGameFragment/app/src/main/java/mx/antonioyee/soccergamefragment/StadiumActivity.java
@@ -1,25 +1,45 @@
 package mx.antonioyee.soccergamefragment;
 
+import android.graphics.ImageFormat;
+import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class StadiumActivity extends AppCompatActivity implements PlayerFragment.PlayerChat {
 
     private FragmentManager dt;
     private int blue, yellow;
+    private TextView txtScoreBlue, txtScoreYellow;
+    private int scoreBlue, scoreYellow, position;
+    private String positionStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stadium);
 
+        if ( savedInstanceState != null ){
+            this.scoreBlue = savedInstanceState.getInt("scoreBlue");
+            this.scoreYellow = savedInstanceState.getInt("scoreYellow");
+            this.positionStr = savedInstanceState.getString("position");
+            this.position = Integer.parseInt(positionStr);
+        }
+
         this.blue = getResources().getColor(R.color.blue);
         this.yellow = getResources().getColor(R.color.yellow);
 
         this.dt = getSupportFragmentManager();
+
+        txtScoreBlue = (TextView) findViewById(R.id.txtScoreBlue);
+        txtScoreYellow = (TextView) findViewById(R.id.txtScoreYellow);
+
+        txtScoreBlue.setText(String.valueOf(scoreBlue));
+        txtScoreYellow.setText(String.valueOf(scoreYellow));
 
         FragmentTransaction ft = dt.beginTransaction();
 
@@ -43,10 +63,16 @@ public class StadiumActivity extends AppCompatActivity implements PlayerFragment
 
     public void startGame(){
         if ( dt != null ){
-            int index = (dt.getFragments().size() > 1)? dt.getFragments().size() - 1: 0;
 
-            PlayerFragment playerFragment = (PlayerFragment) dt.getFragments().get(PlayerFragment.getRandomPosition(0, index));
-            playerFragment.showBall(true);
+            if ( positionStr != null ){
+                PlayerFragment playerFragment = (PlayerFragment) dt.getFragments().get(PlayerFragment.getRandomPosition(0, position));
+                playerFragment.showBall(true);
+            }else{
+                int index = (dt.getFragments().size() > 1)? dt.getFragments().size() - 1: 0;
+
+                PlayerFragment playerFragment = (PlayerFragment) dt.getFragments().get(PlayerFragment.getRandomPosition(0, index));
+                playerFragment.showBall(true);
+            }
         }
     }
 
@@ -59,8 +85,36 @@ public class StadiumActivity extends AppCompatActivity implements PlayerFragment
 
     @Override
     public void passBall(int position) {
+
+        if ( position == 0 ){
+            scoreYellow++;
+        }else if( position == 3 ){
+            scoreBlue++;
+        }
+
+        if ( scoreBlue == 5 || scoreYellow == 5 ){
+            scoreBlue = 0;
+            scoreYellow = 0;
+        }
+
+        this.position = position;
+
+        txtScoreBlue.setText(String.valueOf(scoreBlue));
+        txtScoreYellow.setText(String.valueOf(scoreYellow));
+
         deleteBall();
         PlayerFragment playerFragment = (PlayerFragment) dt.getFragments().get(position);
         playerFragment.showBall(true);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("scoreBlue", scoreBlue);
+        outState.putInt("scoreYellow", scoreYellow);
+        outState.putString("position", String.valueOf(position));
+    }
+
+
 }
