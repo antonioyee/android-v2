51c49d3593f0bd5f11c21522464eff93ffb3f40b
ANTONIO YEE <yee.antonio@gmail.com>
2016-02-25 13:00:09 -0600

    volley and gson

---
+++ PueblosMagicos/app/src/main/java/mx/antonioyee/pueblosmagicos/application/AppController.java
@@ -0,0 +1,35 @@
+package mx.antonioyee.pueblosmagicos.application;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by antonioyee on 25/02/16.
+ */
+public class AppController extends Application {
+
+    private static AppController mInstance;
+    private static RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance(){
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if ( mRequestQueue == null ){
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return  mRequestQueue;
+    }
+
+}
--- PueblosMagicos/app/src/main/java/mx/antonioyee/pueblosmagicos/fragments/LocationFragment.java
+++ PueblosMagicos/app/src/main/java/mx/antonioyee/pueblosmagicos/fragments/LocationFragment.java
@@ -3,26 +3,35 @@ package mx.antonioyee.pueblosmagicos.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import mx.antonioyee.pueblosmagicos.R;
+import mx.antonioyee.pueblosmagicos.application.AppController;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements Response.ErrorListener, Response.Listener<String> {
 
     private GoogleMap gMap;
 
     public LocationFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,7 +40,49 @@ public class LocationFragment extends Fragment {
         SupportMapFragment mapLocation = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapLocation);
         gMap = mapLocation.getMap();
 
+        String townName = "san cristobal de las casas chiapas chiapas";
+        townName = townName.replaceAll("\\s+", "+");
+        getLocation(townName);
+
         return view;
+    }
+
+    private void getLocation(final String townName){
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + townName;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, this, this){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+
+                //params.put("address", townName);
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError{
+                Map<String, String> params = new HashMap<String, String>();
+
+                //params.put("Content-Type", "application/x-www-form-urlencoded");
+                params.put("address", townName);
+                params.put("key", getResources().getString(R.string.google_key));
+
+                return params;
+            }
+        };
+
+        AppController.getInstance().getRequestQueue().add(stringRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Log.e("ERROR", error.toString());
+    }
+
+    @Override
+    public void onResponse(String response) {
+        Log.e("RESPONSE", response);
     }
 
 }
