55463d41901383cb2fefa4ff76f06826d2662c2d
ANTONIO YEE <yee.antonio@gmail.com>
2016-02-26 15:00:26 -0600

    photos town select

--- PueblosMagicos/app/src/main/java/mx/antonioyee/pueblosmagicos/fragments/ImagesTownFragment.java
+++ PueblosMagicos/app/src/main/java/mx/antonioyee/pueblosmagicos/fragments/ImagesTownFragment.java
@@ -13,14 +13,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import mx.antonioyee.pueblosmagicos.R;
 import mx.antonioyee.pueblosmagicos.adapters.ImagesTownRVAdapter;
+import mx.antonioyee.pueblosmagicos.application.AppController;
 import mx.antonioyee.pueblosmagicos.models.MagicTown;
+import mx.antonioyee.pueblosmagicos.responses.venue.Group;
+import mx.antonioyee.pueblosmagicos.responses.venue.Item;
+import mx.antonioyee.pueblosmagicos.responses.venue.Venue;
+import mx.antonioyee.pueblosmagicos.responses.venues.Venues;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,15 +71,18 @@ public class ImagesTownFragment extends Fragment implements ImagesTownRVAdapter.
 
         rvImagesTown = (RecyclerView) view.findViewById(R.id.rvImagesTown);
 
-        for(MagicTown town: MagicTown.getData()){
+        /*for(MagicTown town: MagicTown.getData()){
             paths.add(town.getPathMainPhoto());
-        }
+        }*/
+
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(getContext(), 3);
 
         adapter = new ImagesTownRVAdapter(paths, getActivity(), ImagesTownFragment.this);
 
-        LinearLayoutManager linearLayoutManager = new GridLayoutManager(getContext(), 3);
         rvImagesTown.setLayoutManager(linearLayoutManager);
         rvImagesTown.setAdapter(adapter);
+
+        getVenues(24.809065, -107.394012);
 
         return view;
     }
@@ -92,4 +112,108 @@ public class ImagesTownFragment extends Fragment implements ImagesTownRVAdapter.
         outState.putBoolean("show", show);
         outState.putString("path", path);
     }
+
+    public void getVenues(double lat, double log){
+        String url = "https://api.foursquare.com/v2/venues/search?client_id=" + getResources().getString(R.string.foursquare_client_id)
+                + "&client_secret=" + getResources().getString(R.string.foursquare_client_secret)
+                + "&v=20130815&ll=" + lat + "," + log;
+
+        StringRequest requestVenues = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Gson gson = new Gson();
+                            Type listType = new TypeToken<List<Venues>>(){}.getType();
+                            jsonObject = jsonObject.getJSONObject("response");
+                            List<Venues> venues = (List<Venues>) gson.fromJson(jsonObject.get("venues").toString(),listType);
+
+                            for(Venues venue :venues){
+                                getPhotos(venue.getId());
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("DEBUG", error.toString());
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                //params.put("address", townName);
+                params.put("key", getResources().getString(R.string.google_key));
+
+                return params;
+            }
+        };
+
+        AppController.getInstance().getRequestQueue().add(requestVenues);
+    }
+
+    public void getPhotos(String venue_id){
+        String url = "https://api.foursquare.com/v2/venues/"+venue_id+"?client_id="+getResources().getString(R.string.foursquare_client_id)+"&client_secret="+getResources().getString(R.string.foursquare_client_secret)+"&v=20160225";
+
+        StringRequest requestVenues = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Gson gson = new Gson();
+                            Venue venue = gson.fromJson(jsonObject.getJSONObject("response").getJSONObject("venue").toString(), Venue.class);
+
+                            if ( venue.getPhotos().getCount() > 0 ){
+                                Group group = venue.getPhotos().getGroups().get(0);
+
+                                Item item = group.getItems().get(0);
+
+                                String path = item.getPrefix() + "400x400" +item.getSuffix();
+
+                                paths.add(path);
+                                adapter.notifyDataSetChanged();
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("DEBUG", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                //params.put("address", townName);
+                params.put("key", getResources().getString(R.string.google_key));
+
+                return params;
+            }
+        };
+
+        AppController.getInstance().getRequestQueue().add(requestVenues);
+    }
+
 }
